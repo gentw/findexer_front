c43a312d9617a6bad9366eff9c3d6da6d5a30379
gentw <gentiannuka@gmail.com>
2023-01-18 14:23:15 +0100

    Disabling Theme mode switcher

--- src/layouts/main-layout/header/Topbar.vue
+++ src/layouts/main-layout/header/Topbar.vue
@@ -23,27 +23,7 @@
       </div>
       <!--end::Activities-->
 
-      <!--begin::Theme mode-->
-      <div class="d-flex align-items-center ms-3 ms-lg-4">
-        <!--begin::Menu toggle-->
-        <a
-          href="#"
-          class="btn btn-icon btn-color-gray-700 btn-active-color-primary btn-outline w-40px h-40px"
-          data-kt-menu-trigger="{default:'click', lg: 'hover'}"
-          data-kt-menu-attach="parent"
-          data-kt-menu-placement="bottom-end"
-        >
-          <span class="svg-icon theme-light-show svg-icon-1">
-            <inline-svg src="media/icons/duotune/general/gen060.svg" />
-          </span>
-          <span class="svg-icon theme-dark-show svg-icon-1">
-            <inline-svg src="media/icons/duotune/general/gen061.svg" />
-          </span>
-        </a>
-        <!--begin::Menu toggle-->
-        <KTThemeModeSwitcher />
-      </div>
-      <!--end::Theme mode-->
+    
 
       <button
         id="kt_sidebar_toggler"
@@ -61,13 +41,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import KTSearch from "@/layouts/main-layout/header/partials/Search.vue";
-import KTThemeModeSwitcher from "@/layouts/main-layout/theme-mode/ThemeModeSwitcher.vue";
 
 export default defineComponent({
     name: "LayoutTopbar",
     components: {
         KTSearch,
-        KTThemeModeSwitcher,
     },
 });
 </script>
